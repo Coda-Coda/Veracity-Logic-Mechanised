45315247c9b7ddb8dc272fed0993fdbd21cdbb84
Daniel Britten <daniel.britten@outlook.com>
2023-12-11 17:05:34 +1300

    Changed to using notations with recursive patterns https://coq.inria.fr/refman/user-extensions/syntax-extensions.html#notations-with-recursive-patterns

--- Coq/generateNotations.py
+++ Coq/generateNotations.py
@@ -4,21 +4,15 @@
 
 import functools
 
-def notationAB(n):
-  return 'Notation "A ' + '-'*n + ' B" := (A -> B) (at level 95, right associativity).\n'
-
-def notationABC(n):
-  return 'Notation "A | B ' + '-'*n + ' C" := ((A /\ B) -> C) (at level 94, right associativity).\n'
-
-def notationABCD(n):
-  return 'Notation "A | B | C ' + '-'*n + ' D" := ((A /\ B /\ C) -> D) (at level 93, right associativity).\n'
+def notationLine(n):
+  return 'Notation "{ x | .. | y } ' + '-'*n + '  C" := ((and x .. (and y True) ..) -> C) (at level 99, right associativity).\n'
 
 def flatten(s):
   return functools.reduce(lambda a, b : a + b, s)
 
 def notationCommands(minDashes, maxDashes):
-  return flatten(map(notationAB, range(minDashes, maxDashes + 1))) + "\n" + (flatten(map(notationABC, range(minDashes, maxDashes + 1)))) + "\n" + flatten(map(notationABCD, range(minDashes, maxDashes + 1)))
+  return flatten(map(notationLine, range(minDashes, maxDashes + 1)))
 
 minDashes = 3
-maxDashes = 50
+maxDashes = 100
 print(notationCommands(minDashes, maxDashes))
