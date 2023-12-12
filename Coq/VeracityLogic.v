cbc976fcc8453f066e33b39b0be0ef6626a07a2c
Daniel Britten <daniel.britten@outlook.com>
2023-12-12 17:15:33 +1300

    Add link to with-weights branch

--- Coq/VeracityLogic.v
+++ Coq/VeracityLogic.v
@@ -1,4 +1,5 @@
 (** See also VeracityLogicV2.v - https://github.com/Coda-Coda/Veracity-Logic-Mechanised/blob/main/Coq/VeracityLogicV2.v *)
+(** And an initial version with weights - https://github.com/Coda-Coda/Veracity-Logic-Mechanised/blob/with-weights/Coq/VeracityLogicV2.v *)
 
 Require Import List.
 Import ListNotations.
