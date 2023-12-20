b9eb94876a7f4cd2af81bf6145e58754426e6590
Daniel Britten <daniel.britten@outlook.com>
2023-12-20 19:22:35 +1300

    Add proof rule to capture "from the world idea" This helps make it possible to keep Entail being an inductive proposition, which helps when proving negations. Also added a section variable (proposition) called FromTheWorld  to facilitate this.

--- Coq/VeracityLogic.v
+++ Coq/VeracityLogic.v
@@ -26,7 +26,7 @@ Inductive sentence :=
 
 Infix "/\'" := And (at level 80, right associativity).
 Infix "\/'" := Or (at level 85, right associativity). 
-Notation "A : E \in C" := (Judgement A E C) (at level 1).
+Notation "A :' E \in C" := (Judgement A E C) (at level 1).
 Notation "_|_" := (Bottom) (at level 1).
 Notation "( x , y , .. , z )" := (Pair .. (Pair x y) .. z).
 
@@ -129,68 +129,77 @@ Notation "A --------------------------------------------------------------------
 Notation "A --------------------------------------------------------------------------------------------------- B" := (A -> B) (at level 99, right associativity, only parsing).
 Notation "A ---------------------------------------------------------------------------------------------------- B" := (A -> B) (at level 99, right associativity, only parsing).
 
+
+Context (FromTheWorld : nat -> evid -> claim -> Prop).
+
 Reserved Notation "A |- B" (at level 80).
 
 Inductive Entail : list sentence -> sentence -> Prop :=
-| bot_elim : forall Ps a e C,
+| from_the_world Ps a e C
 
-                      (Ps |- (a : e \in _|_))
-                      -----------------------
-                       (Ps |- (a : e \in C))
+                       (_ : (FromTheWorld a e C))
+                                  :
+                        (Ps |- (a :' e \in C))
+                     
+| bot_elim Ps a e C
 
-| and_intro : forall Ps a e1 e2 C1 C2,
+                      (_: (Ps |- (a :' e \in _|_)))
+                                 :
+                       (Ps |- (a :' e \in C))
 
-               Ps |- (a : e1 \in C1) /\ Ps |- (a : e2 \in C2)
-               ------------------------------------------------
-                   Ps |- (a : (e1, e2) \in (C1 /\' C2))
+| and_intro Ps a e1 e2 C1 C2
 
-| and_elim1 : forall Ps a e1 e2 C1 C2,
+         (_: Ps |- (a :' e1 \in C1)) (_: Ps |- (a :' e2 \in C2)) 
+                                       :
+                   Ps |- (a :' (e1, e2) \in (C1 /\' C2))
 
-                Ps |- (a : (e1, e2) \in (C1 /\' C2))
-                -------------------------------------
-                       Ps |- (a : e1 \in C1)
+| and_elim1 Ps a e1 e2 C1 C2
 
-| and_elim2 : forall Ps a e1 e2 C1 C2,
+                (_:Ps |- (a :' (e1, e2) \in (C1 /\' C2)))
+                                :
+                       Ps |- (a :' e1 \in C1)
 
-                Ps |- (a : (e1, e2) \in (C1 /\' C2))
-                -------------------------------------
-                       Ps |- (a : e2 \in C2)
+| and_elim2 Ps a e1 e2 C1 C2
 
-| or_intro1 : forall Ps a e1 C1 C2,
+                (_: Ps |- (a :' (e1, e2) \in (C1 /\' C2)))
+                                :
+                       Ps |- (a :' e2 \in C2)
 
-                         Ps |- (a : e1 \in C1)
-                  ------------------------------------
-                  Ps |- (a : Left e1 \in (C1 \/' C2))
+| or_intro1 Ps a e1 C1 C2
 
-| or_intro2 : forall Ps a e2 C1 C2,
+                         (_: Ps |- (a :' e1 \in C1))
+                               :
+                  Ps |- (a :' Left e1 \in (C1 \/' C2))
 
-                         Ps |- (a : e2 \in C2)
-                  -------------------------------------
-                  Ps |- (a : Right e2 \in (C1 \/' C2))
+| or_intro2 Ps a e2 C1 C2
 
-| or_elim1 : forall Ps a e1 C1 C2,
+                         (_: Ps |- (a :' e2 \in C2))
+                                :
+                  Ps |- (a :' Right e2 \in (C1 \/' C2))
 
-                  Ps |- (a : Left e1 \in (C1 \/' C2))
-                  ------------------------------------
-                         Ps |- (a : e1 \in C1)
+| or_elim1 Ps a e1 C1 C2
 
-| or_elim2 : forall Ps a e2 C1 C2,
+                  (_: Ps |- (a :' Left e1 \in (C1 \/' C2)))
+                                  :
+                         Ps |- (a :' e1 \in C1)
 
-                  Ps |- (a : Right e2 \in (C1 \/' C2))
-                  ------------------------------------
-                          Ps |- (a : e2 \in C2)
+| or_elim2 Ps a e2 C1 C2
 
-| trust : forall Ps a1 a2 e C,
+                  (_: Ps |- (a :' Right e2 \in (C1 \/' C2)))
+                                    :
+                          Ps |- (a :' e2 \in C2)
 
-                  Ps |- (a2 : e \in C) /\ Ps |- (Trusts a1 a2)
-                  --------------------------------------------
-                          Ps |- (a1 : e \in C)
+| trust Ps a1 a2 e C
 
-| impl_intro : forall Ps Qs a e1 e2 C1 C2,
+                  (_: Ps |- (a2 :' e \in C))  (_: Ps |- (Trusts a1 a2))
+                                    :
+                          Ps |- (a1 :' e \in C)
 
-                  Ps ++ ((a : e1 \in C1) :: Qs) |- (a : e2 \in C2)
-                ----------------------------------------------------
-                (Ps ++ Qs) |- (a : Lambda e1 e2 \in (Implies C1 C2))
+| impl_intro Ps Qs a e1 e2 C1 C2
+
+                  (_: Ps ++ ((a :' e1 \in C1) :: Qs) |- (a :' e2 \in C2))
+                                       :
+                (Ps ++ Qs) |- (a :' Lambda e1 e2 \in (Implies C1 C2))
              
 where "A |- B"  := (Entail A B).
 
@@ -198,32 +207,55 @@ where "A |- B"  := (Entail A B).
 
 Lemma example1 : forall a e1 e2 e3 C1 C2 C3,
 
-   [] |- (a : e1 \in C1) /\ [] |- (a : e2 \in C2) /\ [] |- (a : e3 \in C3)
+   [] |- (a :' e1 \in C1) /\ [] |- (a :' e2 \in C2) /\ [] |- (a :' e3 \in C3)
    -----------------------------------------------------------------------
-            [] |- (a : ((e1, e2), e3) \in ((C1 /\' C2) /\' C3))
+            [] |- (a :' ((e1, e2), e3) \in ((C1 /\' C2) /\' C3))
 .
 Proof.
 intros.
 destruct H as [H1 [H2 H3]].
-apply and_intro; split.
-apply and_intro; split.
+apply and_intro.
+apply and_intro.
 all: assumption.
 Qed.
 
 Lemma example2 : forall a1 a2 e1 e2 C1 C2,
   
-   [] |- (a1 : e1 \in C1) /\ [] |- (a2 : e2 \in C2) /\ [] |- (Trusts a2 a1)
+   [] |- (a1 :' e1 \in C1) /\ [] |- (a2 :' e2 \in C2) /\ [] |- (Trusts a2 a1)
    ------------------------------------------------------------------------
-                       [] |- (a2 : (e1, e2) \in (C1 /\' C2))
+                       [] |- (a2 :' (e1, e2) \in (C1 /\' C2))
 .
 Proof.
 intros.
 destruct H as [H1 [H2 H3]].
-apply and_intro; split; [|assumption].
+apply and_intro; [|assumption].
 eapply trust.
-split.
 apply H1.
 apply H3.
 Qed.
 
 (* The final lemma in the Isabelle examples is not true. *)
+
+
+Lemma incorrect_example1 : forall a e C, ~ ([] |- (a :' e \in C)).
+Proof.
+unfold not.
+intros.
+induction H.
+shelve.
+all: contradiction.
+Abort.
+
+Lemma incorrect_example1 : forall a e C,
+  (forall a' e' C', ~ FromTheWorld a' e' C')
+  ->
+  ~ ([] |- (a :' e \in C)).
+Proof.
+intros.
+unfold not.
+intros.
+induction H0.
+unfold not in H.
+eapply H. apply H0.
+all: contradiction.
+Qed.
