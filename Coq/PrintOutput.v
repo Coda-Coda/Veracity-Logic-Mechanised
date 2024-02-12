0e8112e9c93389dd91dd69f7fb91ff8e16fcd10b
Daniel Britten <daniel.britten@outlook.com>
2024-02-12 20:24:00 +1300

    Add command to print allProofsAsString to separate .v file

--- Coq/PrintOutput.v
+++ Coq/PrintOutput.v
@@ -1,1 +1,8 @@
-Require Import VeracityLogicV3.  
+Require Import Veracity.VeracityLogicV3.
+Require Import List.
+Import ListNotations.
+Require Import String.
+Require Import Coq.Strings.Ascii.
+
+
+Eval compute in allProofsAsString.
