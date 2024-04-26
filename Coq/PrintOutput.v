67410f3e223967f9ba9a70e1f1858568e36deb00
Daniel Britten <daniel.britten@outlook.com>
2024-04-26 21:57:47 +1200

    Delete files related to PrintOutput.v

--- Coq/PrintOutput.v
+++
@@ -1,8 +0,0 @@
-Require Import Veracity.VeracityLogicV3.
-Require Import List.
-Import ListNotations.
-Require Import String.
-Require Import Coq.Strings.Ascii.
-
-
-Eval compute in allProofsAsString.
--- Coq/build.sh
+++
@@ -1,4 +0,0 @@
-# Arg 1 - takes the file to be compiled (full path). If not supplied, the .ds file to be compiled should be in the current directory (and the only .ds file in the directory).
-coqdep -f _CoqProject > .coqdeps.d
-coq_makefile -arg "-quiet" -f _CoqProject -o core.make 
-make -f core.make
