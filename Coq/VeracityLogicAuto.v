896a2f16d10020be10f9bee8c31dc52503b6734f
Daniel Britten <daniel.britten@outlook.com>
2024-04-27 11:58:51 +1200

    Move `getAssumptions` to a more prominent place

--- Coq/VeracityLogicAuto.v
+++ Coq/VeracityLogicAuto.v
@@ -252,6 +252,32 @@ match p with
 | trust a1 a2 C name L => computeEvidence _ L
 end.
 
+Fixpoint getAssumptions (j : judgementPart) (p : proofTreeOf j) : list judgementPart := 
+match p with
+| hole _ => []
+| assume e a C => [\by a \in C]
+| bot_elim a C M => getAssumptions _ M
+| and_intro a C1 C2 L R => 
+    getAssumptions _ L ++ getAssumptions _ R 
+| or_intro1 a C1 C2 M => getAssumptions _ M
+| or_intro2 a C1 C2 M => getAssumptions _ M
+| trust a1 a2 C name L => 
+    getAssumptions _ L
+end.
+
+Fixpoint getAssumptionsWithEvidence (j : judgementPart) (p : proofTreeOf j) : list (judgement) := 
+match p with
+| hole j => [(Judgement HoleEvid j)]
+| assume e a C => [Judgement e (\by a \in C)]
+| bot_elim a C M => getAssumptionsWithEvidence _ M
+| and_intro a C1 C2 L R => 
+    getAssumptionsWithEvidence _ L ++ getAssumptionsWithEvidence _ R 
+| or_intro1 a C1 C2 M => getAssumptionsWithEvidence _ M
+| or_intro2 a C1 C2 M => getAssumptionsWithEvidence _ M
+| trust a1 a2 C name L => 
+    getAssumptionsWithEvidence _ L
+end.
+
 (*|
 
 String representations
@@ -635,32 +661,6 @@ Definition isAtomicEvidence (e : evid) : bool :=
 match e with
   | AtomicEvid _ => true
   | _ => false
-end.
-
-Fixpoint getAssumptions (j : judgementPart) (p : proofTreeOf j) : list judgementPart := 
-match p with
-| hole _ => []
-| assume e a C => [\by a \in C]
-| bot_elim a C M => getAssumptions _ M
-| and_intro a C1 C2 L R => 
-    getAssumptions _ L ++ getAssumptions _ R 
-| or_intro1 a C1 C2 M => getAssumptions _ M
-| or_intro2 a C1 C2 M => getAssumptions _ M
-| trust a1 a2 C name L => 
-    getAssumptions _ L
-end.
-
-Fixpoint getAssumptionsWithEvidence (j : judgementPart) (p : proofTreeOf j) : list (judgement) := 
-match p with
-| hole j => [(Judgement HoleEvid j)]
-| assume e a C => [Judgement e (\by a \in C)]
-| bot_elim a C M => getAssumptionsWithEvidence _ M
-| and_intro a C1 C2 L R => 
-    getAssumptionsWithEvidence _ L ++ getAssumptionsWithEvidence _ R 
-| or_intro1 a C1 C2 M => getAssumptionsWithEvidence _ M
-| or_intro2 a C1 C2 M => getAssumptionsWithEvidence _ M
-| trust a1 a2 C name L => 
-    getAssumptionsWithEvidence _ L
 end.
 
 Close Scope string.
